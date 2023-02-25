55385b9124a38615cdaac3f97690021dcfd16344
Subhav Mittal <mittalsubhav@gmail.com>
2023-02-25 23:24:54 +0530

    main.py completed

--- main.py
+++ main.py
@@ -13,21 +13,28 @@ root,numsteps = bisection(2000,2024,China,India,0.5)
 root2,numsteps2 = newton(root,China,India,10,0.5)
 if root2==-1:
     print("Method failed after",10,"steps")
-    print("Lets find the minima of the difference of the 2 polynomials")
-    print("We have to find the root of the derivative of the difference of the 2 polynomials")
-# x_der = []
-# x_der.append((2000,test.get_derivative(2000)))
-# x_der.append((2005,test.get_derivative(2005)))
-# x_der.append((2010,test.get_derivative(2010)))
-# x_der.append((2015,test.get_derivative(2015)))
-# x_dummy = [(2000,0),(2005,0),(2010,0),(2015,0)]
-# check = LagrangePolynomial(4)
-# check.load_data(x_der,x_dummy)
-# check.generate_coefficients()
-# root,numsteps = bisection(2000,2030,check.get_polynomial,0.5)
-# root2,numsteps2 = newton(root,check.get_polynomial,check.get_derivative,10,1e-9)
-# print(root2)
-# print(test.get_polynomial(root2))
+x_der1 = []
+x_der2 = []
+input = [2000,2005,2010,2015]
+for i in input:
+    China.evaluate(i)
+    India.evaluate(i)
+    x_der1.append((i,China.get_derivative()))
+    x_der2.append((i,India.get_derivative()))
+China_der = LagrangePolynomial(4)
+India_der = LagrangePolynomial(4)
+China_der.load_data(x_der1)
+India_der.load_data(x_der2)
+China_der.generate_coefficients()
+India_der.generate_coefficients()
+root,numsteps = bisection(2000,2022,China_der,India_der,0.5)
+print(root)
+root2,numsteps2 = newton(root,China_der,India_der,10,1e-9)
+print(root2)
+print(numsteps2)
+China.evaluate(root2)
+India.evaluate(root2)
+print(China.get_polynomial()-India.get_polynomial())
 
 
 
