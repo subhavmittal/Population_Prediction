d41655d0d061b1b119352c435b7312c2715ba3b2
Subhav Mittal <mittalsubhav@gmail.com>
2023-02-25 17:56:21 +0530

    cleaned the code

--- background_functions.py
+++ background_functions.py
@@ -5,13 +5,58 @@
 # Whereas horner's method takes O(n) time
 import matplotlib.pyplot as plt
 import math
-def bisection(a,b,f,tol=1e-5):
-    FA = f(a)
+class LagrangePolynomial:
+    def __init__(self,n):
+        self.n = n
+        self.x = [0 for i in range(n)]
+        self.fx = [0 for i in range(n)]
+        self.a = [0 for i in range(n)]
+        self.pol = 0
+        self.der = 0
+    def load_data(self,x):
+        for i in range(self.n):
+            # Stores the years as the x values
+            self.x[self.n - 1 - i] = x[i][0]
+            # Stores the population of the country in billions as the y values
+            self.fx[i] = x[i][1]
+    def generate_coefficients(self):
+        # Initializing the divided difference tables for both china and india
+        F = [[0 for j in range(i+1)] for i in range(self.n)]
+        # First column of the table is the f(x) values
+        for i in range(self.n):
+            F[i][0] = self.fx[i]
+        # Generating the divided difference tables recursively    
+        for i in range(1,self.n):
+            for j in range(1,i+1):
+                F[i][j] = (F[i][j-1] - F[i-1][j-1])/(self.x[self.n - 1 - i] - self.x[self.n - 1 - (i-j)])
+        # Storing the coefficients of the polynomials ak [k = n-1 to 0]     
+        for i in range(self.n):
+            self.a[self.n - 1 - i] = F[i][i]
+    def evaluate(self,y):
+        val= self.a[0]
+        der = val
+        for i in range(1,self.n-1):
+            val = val*(y - self.x[i]) + self.a[i]
+            der = der*(y - self.x[i+1]) + val
+        val = val*(y - self.x[self.n-1]) + self.a[self.n - 1]
+        self.pol = val
+        self.der = der
+    def get_polynomial(self):
+        return self.pol
+    def get_derivative(self):
+        return self.der
+
+def bisection(a,b,LagrangePolynomial1,LagrangePolynomial2,tol=1e-5):
+    LagrangePolynomial1.evaluate(a)
+    LagrangePolynomial2.evaluate(a)
+    FA = LagrangePolynomial1.get_polynomial() - LagrangePolynomial2.get_polynomial()
     numsteps = 0
     N0 = math.ceil(math.log(abs((b-a))/tol,2))
     for i in range(N0):
         c = a + (b-a)/2
-        F = f(c)
+        LagrangePolynomial1.evaluate(c)
+        LagrangePolynomial2.evaluate(c)
+        F = LagrangePolynomial1.get_polynomial() - LagrangePolynomial2.get_polynomial()
         numsteps += 1
         if F == 0:
             return c,numsteps
@@ -21,11 +66,13 @@ def bisection(a,b,f,tol=1e-5):
             a = c
             FA = F
     return c,numsteps        
-def newton(p0,f,f_prime,maxiter,tol=1e-5):
+def newton(p0,LagrangePolynomial1,LagrangePolynomial2,maxiter,tol=1e-5):
     i = 0
     while i<maxiter:
-        pol = f(p0)
-        der = f_prime(p0)
+        LagrangePolynomial1.evaluate(p0)
+        LagrangePolynomial2.evaluate(p0)
+        pol = LagrangePolynomial1.get_polynomial() - LagrangePolynomial2.get_polynomial()
+        der = LagrangePolynomial1.get_derivative() - LagrangePolynomial2.get_derivative()
         try:
             p = p0 - pol/der
         except:
@@ -35,60 +82,4 @@ def newton(p0,f,f_prime,maxiter,tol=1e-5):
         p0 = p
         i += 1
     if(i == maxiter):
-        return -1,-1
-class LagrangePolynomial:
-    def __init__(self,n):
-        self.n = n
-        self.x = [0 for i in range(n)]
-        self.fx1 = [0 for i in range(n)]
-        self.fx2 = [0 for i in range(n)]
-        self.a1 = [0 for i in range(n)]
-        self.a2 = [0 for i in range(n)]
-        self.pol = 0
-        self.der = 0
-    def load_data(self,x1,x2):
-        for i in range(self.n):
-            # Stores the years as the x values
-            self.x[self.n - 1 - i] = x1[i][0]
-            # Stores the population of china in billions as the y values
-            self.fx1[i] = x1[i][1]
-            # Stores the population of india in billions as the y values
-            self.fx2[i] = x2[i][1]
-    def generate_coefficients(self):
-        # Initializing the divided difference tables for both china and india
-        F1 = [[0 for j in range(i+1)] for i in range(self.n)]
-        F2 = [[0 for j in range(i+1)] for i in range(self.n)]
-        # First column of the table is the f(x) values
-        for i in range(self.n):
-            F1[i][0] = self.fx1[i]
-            F2[i][0] = self.fx2[i]
-        # Generating the divided difference tables recursively    
-        for i in range(1,self.n):
-            for j in range(1,i+1):
-                F1[i][j] = (F1[i][j-1] - F1[i-1][j-1])/(self.x[self.n - 1 - i] - self.x[self.n - 1 - (i-j)])
-                F2[i][j] = (F2[i][j-1] - F2[i-1][j-1])/(self.x[self.n - 1 - i] - self.x[self.n - 1 - (i-j)])
-        # Storing the coefficients of the polynomials ak [k = n-1 to 0]     
-        for i in range(self.n):
-            self.a1[self.n - 1 - i] = F1[i][i]
-            self.a2[self.n - 1 - i] = F2[i][i]
-    def evaluate(self,y):
-        val_1 = self.a1[0]
-        der_1 = val_1
-        val_2 = self.a2[0]
-        der_2 = val_2
-        for i in range(1,self.n-1):
-            val_1 = val_1*(y - self.x[i]) + self.a1[i]
-            der_1 = der_1*(y - self.x[i+1]) + val_1
-            val_2 = val_2*(y - self.x[i]) + self.a2[i]
-            der_2 = der_2*(y - self.x[i+1]) + val_2
-        val_1 = val_1*(y - self.x[self.n-1]) + self.a1[self.n - 1]
-        val_2 = val_2*(y - self.x[self.n-1]) + self.a2[self.n - 1]
-        self.pol = val_1 - val_2
-        self.der = der_1 - der_2
-    def get_polynomial(self,y):
-        self.evaluate(y)
-        return self.pol
-    def get_derivative(self,y):
-        self.evaluate(y)
-        return self.der
-        
+        return -1,-1    
--- main.py
+++ main.py
@@ -3,33 +3,31 @@ from background_functions import LagrangePolynomial,bisection,newton
 x1 = [(2000,1.28),(2005,1.31),(2010,1.35),(2015,1.39),(2023,1.41)]
 #India population data
 x2 = [(2000,1.04),(2005,1.1),(2010,1.2),(2015,1.32),(2023,1.40)]    
-test = LagrangePolynomial(5)
-test.load_data(x1,x2)    
-test.generate_coefficients()
-print(test.get_polynomial(2000))
-print(test.get_polynomial(2005))
-print(test.get_polynomial(2010))
-print(test.get_polynomial(2015))
-print(test.get_polynomial(2023))
-root,numsteps = bisection(2000,2024,test.get_polynomial,0.5)
-root2,numsteps2 = newton(root,test.get_polynomial,test.get_derivative,10,0.5)
+China = LagrangePolynomial(5)
+China.load_data(x1)    
+China.generate_coefficients()
+India = LagrangePolynomial(5)
+India.load_data(x2)
+India.generate_coefficients()
+root,numsteps = bisection(2000,2024,China,India,0.5)
+root2,numsteps2 = newton(root,China,India,10,0.5)
 if root2==-1:
     print("Method failed after",10,"steps")
     print("Lets find the minima of the difference of the 2 polynomials")
     print("We have to find the root of the derivative of the difference of the 2 polynomials")
-x_der = []
-x_der.append((2000,test.get_derivative(2000)))
-x_der.append((2005,test.get_derivative(2005)))
-x_der.append((2010,test.get_derivative(2010)))
-x_der.append((2015,test.get_derivative(2015)))
-x_dummy = [(2000,0),(2005,0),(2010,0),(2015,0)]
-check = LagrangePolynomial(4)
-check.load_data(x_der,x_dummy)
-check.generate_coefficients()
-root,numsteps = bisection(2000,2030,check.get_polynomial,0.5)
-root2,numsteps2 = newton(root,check.get_polynomial,check.get_derivative,10,1e-9)
-print(root2)
-print(test.get_polynomial(root2))
+# x_der = []
+# x_der.append((2000,test.get_derivative(2000)))
+# x_der.append((2005,test.get_derivative(2005)))
+# x_der.append((2010,test.get_derivative(2010)))
+# x_der.append((2015,test.get_derivative(2015)))
+# x_dummy = [(2000,0),(2005,0),(2010,0),(2015,0)]
+# check = LagrangePolynomial(4)
+# check.load_data(x_der,x_dummy)
+# check.generate_coefficients()
+# root,numsteps = bisection(2000,2030,check.get_polynomial,0.5)
+# root2,numsteps2 = newton(root,check.get_polynomial,check.get_derivative,10,1e-9)
+# print(root2)
+# print(test.get_polynomial(root2))
 
 
 
